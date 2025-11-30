c4ab0d0faee2b75fd18682ffed734f4a935c2c43
Sopitcho <soanne.miguel@gmail.com>
2025-11-30 18:51:45 +0100

    Update script configuration and riddles

--- script.js
+++ script.js
@@ -5,23 +5,23 @@
 
 /* ========== CONFIG ========== */
 // URL de ton Apps Script (WebApp)
-const SCRIPT_URL = "https://script.google.com/macros/library/d/1sH1hIHkW9tjsYdGF5HE8xZv6hYfSdmGu0HcYyePwE7oHsmGzrrCqS7dZ/10";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbweehPoUPK4731aZ27siiFAp_SpSfDKQ3-q58XI0gCBF727lC4LSN_ZqZ0lKEbmq0f04A/exec";
 
-// Heure d'ouverture (ex : 12 = midi)
-const HEURE_OUVERTURE = 12;
+// Heure d'ouverture (10h)
+const HEURE_OUVERTURE = 10;
 
 // Jours (1..19 sans week-ends)
 const JOURS = [1,2,3,4,5,8,9,10,11,12,15,16,17,18,19];
 
 const ENIGMES = {
-  1:"Je commence la journée sans effort et termine souvent en musique. Qui suis-je ?",
-  2:"Qu’est-ce qui a des clés mais n’ouvre aucune porte ?",
-  3:"Énigme 3...",
-  4:"Énigme 4...",
-  5:"Énigme 5...",
-  8:"Énigme 8...",
-  9:"Énigme 9...",
-  10:"Énigme 10...",
+  1:"Grâce à l'intervention de M.Albert, j'ai pu éviter une heure de colle. Qui suis-je ?",
+  2:"En hiver, je porte souvent un bonnet, même s'il m'arrive de le perdre. Qui suis-je ?",
+  3:"Je fais toujours attention à mon style. D'ailleurs, mes lunettes sont souvent accordées à mon pull et à mes basket. Qui suis-je ?",
+  4:"Le plus beau déguisement du carnaval ? C'est moi ! Qui suis-je ?",
+  5:"On ne me voit jamais sans un élève accroché à mes basket. Qui suis-je ?",
+  8:"J'ai un bureau mais je ne suis jamais dedans.. Qui suis-je ?",
+  9:"J'ai battu un élève à Fifa.. il était doué mais je l'ai eu aux pélantys ! Qui suis-je ?",
+  10:"Aujourd'hui défi particulier, pour l'emporter, il faut trouver où se trouve aumônerie et faire une photo de la salle. Bonne chance !",
   11:"Énigme 11...",
   12:"Énigme 12...",
   15:"Énigme 15...",
@@ -52,7 +52,7 @@ const POSITIONS = [
 
 /* ========== MODE TEST & ADMIN ========== */
 /* true = mode test activé (simule jour+heure). Mets false en production */
-let MODE_TEST = true;
+let MODE_TEST = false;
 
 /* jour et heure simulés (si MODE_TEST = true) */
 let JOUR_SIMULE = 5;   // change le jour ici pour tester (ou appuie sur "A")
